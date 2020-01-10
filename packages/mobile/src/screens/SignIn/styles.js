711d4e93d5d5ddef361f88a2f560db97fd1a2a7b
Evandro Ribeiro <ribeiroevandro@live.com>
2020-01-10 19:24:20 -0300

    Created SignIn Screen

---
+++ packages/mobile/src/screens/SignIn/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+
+import { COLORS } from '~/constants';
+
+export const Container = styled.View`
+  flex: 1;
+  background: ${COLORS.BACKGROUND};
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
+
+export const IconContainer = styled.View`
+  margin-bottom: 50px;
+`;
+
+export const Button = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})`
+  height: 44px;
+  align-self: stretch;
+  margin-top: 10px;
+  background-color: ${COLORS.TWITTER};
+  border-radius: 5px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${COLORS.WHITE};
+  font-size: 16px;
+  font-weight: bold;
+`;
