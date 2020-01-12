d6abbc78c82b1af484f2e5ba51fc8fb9859ecae4
Evandro Ribeiro <ribeiroevandro@live.com>
2020-01-12 11:08:06 -0300

    Refactored Tweet component

--- packages/mobile/src/components/Tweet/index.js
+++ packages/mobile/src/components/Tweet/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { api } from '@tclone/services';
 
 import { COLORS } from '~/constants';
 
@@ -13,11 +14,16 @@ import {
 } from './styles';
 
 export default function Tweet({ tweets }) {
+  async function handleLike() {
+    const { _id } = tweets;
+    await api.post(`likes/${_id}`);
+  }
+
   return (
     <Container>
       <Author>{tweets.author}</Author>
       <Content>{tweets.content}</Content>
-      <Button onPress={() => {}}>
+      <Button onPress={handleLike}>
         <ButtonIcon>
           <Ionicons name="ios-heart-empty" size={20} color={COLORS.LIKE} />
         </ButtonIcon>
